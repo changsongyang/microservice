8b0f27a7daa9a5066b297ef4f1f3b142bc6bb960
chenyf <yufeng629@163.com>
2019-01-29 17:55:33 +0800

    完善 nacos-dubbo 的示例模块

--- demo-nacos-dubbo/demo-nacos-provider/src/main/java/org/study/demo/nacos/provider/NacosDubboProviderApp.java
+++ demo-nacos-dubbo/demo-nacos-provider/src/main/java/org/study/demo/nacos/provider/NacosDubboProviderApp.java
@@ -1,14 +1,11 @@
 package org.study.demo.nacos.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NacosDubboProviderApp {
     public static void main(String[] args) {
         SpringApplication.run(NacosDubboProviderApp.class, args);
-//        new SpringApplicationBuilder(NacosDubboProviderApp.class).web(WebApplicationType.NONE).run(args);
     }
 }
